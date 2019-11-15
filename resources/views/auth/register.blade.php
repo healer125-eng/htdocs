7f46ffe8abf5b93c38c25c413c230d02a27577ae
healer125-eng <healer125@outlook.com>
2019-11-14 20:21:57 -0800

    first in htdocs

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,136 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Register</div>
+
+                <div class="panel-body">
+                    <form class="form-horizontal" method="POST" action="{{ route('register') }}">
+                        {{ csrf_field() }}
+
+                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                            <label for="name" class="col-md-4 control-label">Name</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+
+                                @if ($errors->has('name'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('family_name') ? ' has-error' : '' }}">
+                            <label for="family_name" class="col-md-4 control-label">Family Name</label>
+
+                            <div class="col-md-6">
+                                <input id="family_name" type="text" class="form-control" name="family_name" value="{{ old('family_name') }}">
+
+                                @if ($errors->has('family_name'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('family_name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <label for="password" class="col-md-4 control-label">Password</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control" name="password" required>
+
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('phone_num') ? ' has-error' : '' }}">
+                            <label for="phone_num" class="col-md-4 control-label">Phone number</label>
+
+                            <div class="col-md-6">
+                                <input id="phone_num" type="number" class="form-control" name="phone_num" required>
+
+                                @if ($errors->has('phone_num'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('phone_num') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-12 col-md-offset-4">
+                                <label for="phone_num" class="col-md-4 control-label" style="text-align: center; width: 50%;">With registration you accept our
+                                    <a href="#">privacy policy & terms & conditions</a>
+                                </label>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary" style="width: 100%">
+                                    Register
+                                </button>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-12 col-md-offset-4">
+                                <label for="phone_num" class="col-md-4 control-label" style="text-align: center; width: 50%;">or registration with
+                                </label>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary" style="width: 100%">
+                                    Facebook
+                                </button>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary" style="width: 100%">
+                                    Twitter
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/layouts/homepagelayout.blade.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <title>DYNASUN</title>
+    <!-- styles -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+    <link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/animate.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/ionicons.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/owl.carousel.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/magnific-popup.css') }}" rel="stylesheet">
+    <!-- custom css file -->
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <!-- scripts -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('js/easing.min.js') }}"></script>
+    <script src="{{ asset('js/hoverIntent.js') }}"></script>
+    <script src="{{ asset('js/jquery-migrate.min.js') }}"></script>
+    <script src="{{ asset('js/magnific-popup.min.js') }}"></script>
+    <script src="{{ asset('js/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('js/sticky.js') }}"></script>
+    <script src="{{ asset('js/superfish.min.js') }}"></script>
+    <script src="{{ asset('js/wow.min.js') }}"></script>
+    <script src="{{ asset('js/welcome.js') }}"></script>
+  </head>
+<body id="body">
+    <!-- <div id="app"> -->
+    <header id="header">
+      <div id="logo" class="pull-left">
+        <h1><a href="#body" class="scrollto"><img class="logo_img" src="{{ asset('img/logo_big.jpg') }}"></a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="#body"><img src="img/logo.png" alt="" title="" /></a>-->
+      </div>
+
+      <nav id="nav-menu-container">
+        <ul class="nav-menu">
+          @if (Route::has('login'))
+            @if (Auth::check())
+                <li><a href="{{ url('/home') }}">About Us</a></li>
+            @else
+              <li><a href="{{ url('/login') }}">login</a></li>
+              <li><a href="{{ url('/register') }}">register</a></li>
+              <li class="menu-has-children"><a href="#"><img class="flag" src="{{ asset('img/flag logo/en.png') }}"></a>
+                <ul>
+                  <li><a href="#"><img class="flag" src="{{ asset('img/flag logo/fr.png') }}"></a></li>
+                  <li><a href="#"><img class="flag" src="{{ asset('img/flag logo/it.png') }}"></a></li>
+                  <li><a href="#"><img class="flag" src="{{ asset('img/flag logo/de.png') }}"></a></li>
+                  <li><a href="#"><img class="flag" src="{{ asset('img/flag logo/es.png') }}"></a></li>
+                </ul>
+              </li>
+              @endif
+          @endif
+        </ul>
+      </nav><!-- #nav-menu-container -->
+    </header>
+
+    @yield('content')
+    <!-- </div> -->
+    <footer id="footer">
+      <div class="container">
+        <div class="copyright">
+          &copy; Copyright <strong>William</strong>. All Rights Reserved
+        </div>
+        <div class="credits">
+        </div>
+      </div>
+    </footer>
+    <!-- #footer -->
+    <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+</body>
+</html>
